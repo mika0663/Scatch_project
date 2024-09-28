322f5c527977dbfcdcb9a895ee03e5adae6f805d
Muhammad Ikram <mikrams0663@gmail.com>
2024-09-28 15:36:21 -0700

    routes setting is done working with postman desktop, and owner Model is also created

--- app.js
+++ app.js
@@ -3,12 +3,22 @@ const app = express();
 
 const cookieParser = require('cookie-parser');
 const path = require('path');
+const db = require("./configs/mongoose-connection");
+const ownersRouter = require('./routes/ownersRouter');
+const productsRouter = require('./routes/productRouter');
+const usersRouter = require('./routes/usersRouter');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
+app.use("/owners",ownersRouter);
+app.use("/users",usersRouter);
+app.use("/products",productsRouter);
+
+
+
 
 
 const port = 3000
