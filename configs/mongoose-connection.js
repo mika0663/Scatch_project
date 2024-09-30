4c3d00a72ec4c176b8aa41e7d22c7ac5c0500fd9
Muhammad Ikram <mikrams0663@gmail.com>
2024-09-30 10:05:15 -0700

    setting the debugger and environment variables(pending)

--- configs/mongoose-connection.js
+++ configs/mongoose-connection.js
@@ -1,9 +1,10 @@
 const mongoose =require('mongoose');
+const dbgr = require('debug')("development:mongoose");
 
 mongoose
 .connect("mongodb://127.0.0.1:27017/scatch")
 .then(function(){
-    console.log("connected");
+    dbgr("connected");
 })
 .catch(function(err){
     console.log(err);
