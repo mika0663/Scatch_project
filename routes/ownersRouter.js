4912edf58b2eebe4d0030ccfaab1a3f513aa357e
Muhammad Ikram <mikrams0663@gmail.com>
2024-10-02 21:34:01 +0500

    making owner routes

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const ownerModel = require("../models/ownerModel");
+
+
 
 router.get("/", function(req,res){
     res.send("Hey its working");
 })
 
+
+
+
+router.post("/create", function(req,res){
+    res.send("working");
+})
 
 module.exports = router;
